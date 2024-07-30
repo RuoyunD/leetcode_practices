3810c97e340ad0e05a058b485a55ac7b666dd1b4
R.D. <67073146+RuoyunD@users.noreply.github.com>
2024-07-30 10:20:17 +0800

    Update lc_21_mergeTwoLists.py

--- lc_21_mergeTwoLists.py
+++ lc_21_mergeTwoLists.py
@@ -25,4 +25,14 @@ class Solution:
                     l2 = l2.next
             head = head.next
         return res.next
-    
+    #递归
+def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        list2.next = self.mergeTwoLists(list1, list2.next)
+        return list2
