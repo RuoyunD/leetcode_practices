d59945ee946ffec1c120bb82c7103b973276544b
R.D. <67073146+RuoyunD@users.noreply.github.com>
2024-07-30 09:31:06 +0800

    Update lc_448_findDisappearedNumbers.py

--- lc_448_findDisappearedNumbers.py
+++ lc_448_findDisappearedNumbers.py
@@ -1,6 +1,8 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        # 不使用哈希达到空间复杂度O(1)
         for i in range(0, len(nums)):
+            # 对出现过的数字进行标记：-1后变为负数
             if nums[i] > 0:
                 if nums[nums[i]-1] > 0:
                     nums[nums[i]-1] = -nums[nums[i]-1]
