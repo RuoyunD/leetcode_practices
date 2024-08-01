30c77e9ec6b3000e385d340bfd93c6c4aa79a134
R.D. <67073146+RuoyunD@users.noreply.github.com>
2024-08-01 15:55:23 +0800

    Create lc_94_inorderTraversal.py

---
+++ lc_94_inorderTraversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+  # 递归
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        if root == None:
+            return res
+        res.extend(self.inorderTraversal(root.left))
+        res.append(root.val)
+        res.extend(self.inorderTraversal(root.right))
+        return res
+    # 循环迭代
+      def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = deque()
+        res = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            res.append(root.val)
+            root = root.right
+        return res
